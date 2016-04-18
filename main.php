57482a0e13728f531a19f8a1c50d2fe63b4eb1f1
Karol Herbst <git@karolherbst.de>
2016-04-18 15:01:34 +0200

    add single file mode

--- main.php
+++ main.php
@@ -1,9 +1,26 @@
+<?php
+require "config.php";
+?>
 <!DOCTYPE html>
 <html>
  <head>
   <title>Steam Dev Games Overview!</title>
-  <link rel="stylesheet" type="text/css" href="main.css"/>
-  <script src="main.js"></script>
+<?php
+if (!$single_file)
+{
+	echo '<link rel="stylesheet" type="text/css" href="main.css"/>' . PHP_EOL;
+	echo '<script src="main.js"></script>' . PHP_EOL;
+}
+else
+{
+	echo '<style>' . PHP_EOL;
+	readfile("main.css");
+	echo '</style>' . PHP_EOL;
+	echo '<script>' . PHP_EOL;
+	readfile("main.js");
+	echo '</script>' . PHP_EOL;
+}
+?>
  </head>
  <body>
   <?php
--- program.php
+++ program.php
@@ -1,5 +1,4 @@
 <?php
-require "config.php";
 require "game.php";
 
 $game_list = array();
