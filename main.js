10a3388e90111bf3b3870a3352cbe2ef24a5a009
Karol Herbst <git@karolherbst.de>
2016-04-19 00:27:37 +0200

    deal with stupid chars in search field

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ function performSearch(input)
 		var child = dataSet.children[i];
 		var match = false;
 		var keys = input.getAttribute("data-keys").split(" ");
+		var s = input.value.toLowerCase().replace(/[.'\"]/g, "");
 
 		for (var j = 0; j < keys.length; j++) {
 			var key = child.getAttribute("data-" + keys[j]);
-			if (key && key.toLowerCase().indexOf(input.value.toLowerCase()) != -1) {
+			if (key && key.toLowerCase().indexOf(s) != -1) {
 				match = true;
 				break;
 			}
