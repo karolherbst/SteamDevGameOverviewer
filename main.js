0da39e5e6cf19eb4370eab184d401217f691ff81
Karol Herbst <kherbst@redhat.com>
2021-12-06 20:25:24 +0100

    use dataset instead of getAttribute

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function performSearch(i)
 	var coop = document.getElementById("coop-check");
 	var mp = document.getElementById("mp-check");
 
-	var dataSet = document.getElementById(input.getAttribute("data-set"));
-	var keys = input.getAttribute("data-keys").split(" ");
+	var dataSet = document.getElementById(input.dataset.set);
+	var keys = input.dataset.keys.split(" ");
 	var s = input.value.toLowerCase().replace(/[.'\":]/g, "");
 
 	for (i = 0; i < dataSet.childElementCount; ++i)
@@ -13,13 +13,13 @@ function performSearch(i)
 		var child = dataSet.children[i];
 		var match = false;
 
-		var isCoop = child.getAttribute("data-coop") != null;
-		var isMp = child.getAttribute("data-mp") != null;
+		var isCoop = child.dataset.coop != null;
+		var isMp = child.dataset.mp != null;
 
 		if (!((coop.checked && !isCoop) ||
 		      (mp.checked && !isMp))) {
 			for (var j = 0; j < keys.length; j++) {
-				var key = child.getAttribute("data-" + keys[j]);
+				var key = child.dataset[keys[j]];
 				if (key && key.toLowerCase().indexOf(s) != -1) {
 					match = true;
 					break;
